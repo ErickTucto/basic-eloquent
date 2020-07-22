d2bcc9e07dcf5e4ecc3b4f20ce1acf20db9ee337
Erick Tucto <erick.tucto@outlook.com>
2020-07-21 23:52:06 -0500

    Test select query

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class DatabaseTest extends TestCase
+{
+	/** @test */
+	function get_name_to_table_users_with_query_builder()
+	{
+		$capsule = new Capsule;
+		$capsule->addConnection(config('app.database'));
+		$user = $capsule->getConnection()->table('users')->where('id', 1)->get();
+		$this->assertEquals('erick', $user->first()->name);
+	}
+}
