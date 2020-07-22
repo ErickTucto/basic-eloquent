dc7740d526cdeaa75980cefd30399a2465901be9
Erick Tucto <erick.tucto@outlook.com>
2020-07-21 23:16:29 -0500

    helper config

--- helpers.php
+++ helpers.php
@@ -2,7 +2,13 @@
 
 if(!function_exists('config')) {
 	function config(string $get) {
-		$config = require_once __DIR__."/config/app.php";
-		var_dump($config);
+		$get = explode(".", $get);
+		$config = include __DIR__."/config/{$get[0]}.php";
+		if (count($get) > 1) {
+			for ($i=1; $i < count($get); $i++) {
+				$config = $config[$get[$i]];
+			}
+		}
+		return $config;
 	}
 }
--- tests/Helpers/ConfigTest.php
+++ tests/Helpers/ConfigTest.php
@@ -9,7 +9,9 @@ class ConfigTest extends TestCase
 	/** @test */
 	function get_config_by_dots()
 	{
-		$this->markTestIncomplete("Hacer uno mas sencillo antes");
+		$this->assertIsArray(config("app.database"));
+		$this->assertTrue(array_key_exists("host", config("app.database")));
+		$this->assertEquals("localhost", config("app.database.host"));
 	}
 
 	/** @test */
