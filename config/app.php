2282cbdc5c276bc1c42dc0a654776c28605299fb
Erick Tucto <erick.tucto@outlook.com>
2020-07-23 13:09:56 -0500

    Settings project

--- config/app.php
+++ config/app.php
@@ -2,13 +2,13 @@
 
 return [
     'database' => [
-        'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'database'  => 'basic_eloquent',
-        'username'  => 'root',
-        'password'  => '',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
+        'driver'    => env('DB_DRIVE', 'mysql'),
+        'host'      => env('DB_HOST', 'localhost'),
+        'database'  => env('DB_DATABASE', 'basic_eloquent'),
+        'username'  => env('DB_USER', 'root'),
+        'password'  => env('DB_PASSWORD', ''),
+        'charset'   => env('DB_CHARSET', 'utf8'),
+        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+        'prefix'    => env('DB_PREFIX', ''),
     ],
 ];
