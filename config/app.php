a80dce1bf0916ba57866c961449912efa2f091dd
Erick Tucto <erick.tucto@outlook.com>
2020-07-24 08:15:05 -0500

    Add environment variable

--- config/app.php
+++ config/app.php
@@ -2,13 +2,13 @@
 
 return [
     'database' => [
-        'driver'    => env('DB_DRIVE', 'mysql'),
-        'host'      => env('DB_HOST', 'localhost'),
-        'database'  => env('DB_DATABASE', 'basic_eloquent'),
-        'username'  => env('DB_USER', 'root'),
-        'password'  => env('DB_PASSWORD', ''),
-        'charset'   => env('DB_CHARSET', 'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-        'prefix'    => env('DB_PREFIX', ''),
+        'driver'    => $_ENV['DB_DRIVE'] ?? 'mysql',
+        'host'      => $_ENV['DB_HOST'] ?? 'localhost',
+        'database'  => $_ENV['DB_DATABASE'] ?? 'basic_eloquent',
+        'username'  => $_ENV['DB_USER'] ?? 'root',
+        'password'  => $_ENV['DB_PASSWORD'] ?? '',
+        'charset'   => $_ENV['DB_CHARSET'] ?? 'utf8',
+        'collation' => $_ENV['DB_COLLATION'] ?? 'utf8_unicode_ci',
+        'prefix'    => $_ENV['DB_PREFIX'] ?? '',
     ],
 ];
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 
 require __DIR__."/../vendor/autoload.php";
+
+use Symfony\Component\Dotenv\Dotenv;
+
+
+$dotenv = new Dotenv();
+
+$dotenv->load(__DIR__."/../.env");
